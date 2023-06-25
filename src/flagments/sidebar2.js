b190349028782052f5bd2905d59419e358ae9947
Agon Riferd <traehnogardretaw@gmail.com>
2023-06-25 12:34:58 +0700

    NOthing

---
+++ src/flagments/sidebar2.js
@@ -0,0 +1,15 @@
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+export const SideBar = () => {
+    return (
+        <ProSidebar>
+            <Menu iconShape="square">
+                <MenuItem>Menu 1</MenuItem>
+                <SubMenu title="Menu 2">
+                    <MenuItem>Submenu 2.1</MenuItem>
+                    <MenuItem>Submenu 2.2</MenuItem>
+                </SubMenu>
+            </Menu>
+        </ProSidebar>
+    )
+}
