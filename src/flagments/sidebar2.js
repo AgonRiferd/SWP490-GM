d9a1d421b3a4dfd453d64bf07ba91ab129b140c0
Agon Riferd <traehnogardretaw@gmail.com>
2023-06-25 12:44:23 +0700

    Signed-off-by: Agon Riferd <traehnogardretaw@gmail.com>

--- src/flagments/sidebar2.js
+++ src/flagments/sidebar2.js
@@ -1,8 +1,8 @@
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 export const SideBar = () => {
     return (
-        <ProSidebar>
+        <Sidebar>
             <Menu iconShape="square">
                 <MenuItem>Menu 1</MenuItem>
                 <SubMenu title="Menu 2">
@@ -10,6 +10,6 @@ export const SideBar = () => {
                     <MenuItem>Submenu 2.2</MenuItem>
                 </SubMenu>
             </Menu>
-        </ProSidebar>
+        </Sidebar>
     )
 }
