b396e9a7deb575b29ceef68a512639862945b048
Agon Riferd <traehnogardretaw@gmail.com>
2023-08-05 09:57:25 +0700

    Update 04

--- src/components/foods/dialog.js
+++ src/components/foods/dialog.js
@@ -10,6 +10,36 @@ export const Create = ({ onClose }) => {
 export const View = ({ data, onClose }) => {
     return (
         <>
+            <div>
+                <table className='dialog-field'>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label>Tên thực phẩm</label>
+                            </td>
+                            <td>
+                                <span>{data.name}</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Số lượng</label>
+                            </td>
+                            <td>
+                                <span>{data.ammount} {data.unitOfMesuament}</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Năng lượng (Cal)</label>
+                            </td>
+                            <td>
+                                <span>{data.calories}</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <div className="dialog-button-tray">
                 <button type="button" className="any-button button-cancel" onClick={onClose}>
                     Hủy bỏ
