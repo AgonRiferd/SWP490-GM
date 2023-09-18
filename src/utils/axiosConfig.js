efd3aac9244ce024d37b82c9a769e8d34ba2a35b
Agon Riferd <traehnogardretaw@gmail.com>
2023-09-18 23:46:21 +0700

    Update API

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://egts.azurewebsites.net/api',
+    baseURL: 'https://egts1.azurewebsites.net/api',
 });
 
 export default axiosInstance;
